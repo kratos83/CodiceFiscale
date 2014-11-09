0420df147a1f5a419e68b8ced5345f681f936e7e
angelo <angelo.scarna1@codelinsoft.it>
2014-11-09 19:00:54 +0100

    Add windows command

--- update/update.cpp
+++ update/update.cpp
@@ -190,8 +190,11 @@ void update::install_package(){
 
 void update::display_progress_bar()
 {
+#ifdef Q_OS_LINUX
     int val = lin_start->readLine().toInt();
-
+#elif Q_OS_WIN
+    int val = win_start->readLine().toInt()
+#endif
     for(val=0;val <= 100; val++){
         unzip_file->setValue(val);
     }
